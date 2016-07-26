e425c6845388c8d497dee6ce42fde025db280b27
Nengyun Zhang <nengyun.zhang@gmail.com>
2016-07-26 12:24:18 -0700

    multi-regression, gradient descent done

--- src/regression/multiple_linear_regression.py
+++ src/regression/multiple_linear_regression.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LinearRegression
 from math import log
+from math import sqrt
 
 dtype_dict = {'bathrooms': float, 'waterfront': int, 'sqft_above': int, 'sqft_living15': float,
               'grade': int, 'yr_renovated': int, 'price': float, 'bedrooms': float,
@@ -18,7 +19,10 @@ data = pd.read_csv('../../data/regression/kc_house_data.csv', dtype=dtype_dict)
 train_data, test_data = train_test_split(data, test_size=0.2, random_state=0)
 
 
+# *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.* #
 # use build-in linear regression model
+# *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.* #
+
 example_features = ['sqft_living', 'bedrooms', 'bathrooms']
 example_model = LinearRegression(fit_intercept=True)
 example_model.fit(train_data[example_features], train_data['price'])
@@ -85,3 +89,94 @@ model_1_test_rss = get_residual_sum_of_squares(model_1, test_data[model_1_featur
 model_2_test_rss = get_residual_sum_of_squares(model_2, test_data[model_2_features], test_data['price'])
 model_3_test_rss = get_residual_sum_of_squares(model_3, test_data[model_3_features], test_data['price'])
 # print model_1_test_rss, model_2_test_rss, model_3_test_rss
+
+
+# *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.* #
+# Estimating multiple regression coefficients using gradient descent
+# *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.* #
+
+# feautres/output: column names
+def get_numpy_data(dataframe, features, output):
+    dataframe['constant'] = 1
+    features = ['constant'] + features
+    feature_matrix = dataframe[features].as_matrix()
+    output_array = dataframe[output].as_matrix()
+    return feature_matrix, output_array
+
+example_features, example_output = get_numpy_data(data, ['sqft_living'], 'price')
+# print example_features[0, :], example_output[0]
+
+
+# feature_matrix: NxD, weights: Dx1
+def predict_outcome(feature_matrix, weights):
+    predictions = feature_matrix.dot(weights)
+    return predictions
+
+example_weights = np.array([1., 1.])
+example_predictions = predict_outcome(example_features, example_weights)
+# print example_predictions[0:2]
+
+
+# RSS_gradient = -2*H.T*(y-y_hat) -> Dx1 vector
+# errors: y - y_hat, feature: h
+# errors: Nx1, feature: NxD
+def feature_derivative(errors, feature):
+    return -2*feature.T.dot(errors)
+
+example_weights = np.array([0., 0.])
+example_predictions = predict_outcome(example_features, example_weights)
+example_errors = example_predictions - example_output
+feature_const = example_features[:, 0]
+example_derivative = feature_derivative(example_errors, feature_const)
+# print example_derivative, -2*example_output.sum()
+
+
+# use gradient descent to find the weights
+# to find the minimum, update in the direct inverse to the gradient
+def regression_gradient_descent(feature_matrix, output, init_weights, step_size, tolerance):
+    converged = False
+    weights = np.array(init_weights)
+
+    while not converged:
+        predictions = predict_outcome(feature_matrix, weights)
+        errors = output - predictions
+        derivative = feature_derivative(errors, feature_matrix)
+        weights -= step_size*derivative
+
+        gradient_magnitude = sqrt((derivative*derivative).sum())
+        if gradient_magnitude < tolerance:
+            converged = True
+
+    return weights
+
+
+input_features = ['sqft_living']
+example_feature_matrix, example_output = get_numpy_data(train_data, input_features, 'price')
+init_weights = np.array([-47000., 1.])
+step_size = 7e-12
+tolerance = 2.5e7
+example_weights = regression_gradient_descent(example_feature_matrix, example_output, init_weights, step_size, tolerance)
+# print example_weights
+
+input_features = ['sqft_living', 'sqft_living15']
+output_feature = 'price'
+feature_matrix, output = get_numpy_data(train_data, input_features, output_feature)
+init_weights = np.array([-100000., 1., 1.])
+step_size = 4e-12
+tolerance = 1e9
+model_weights = regression_gradient_descent(feature_matrix, output, init_weights, step_size, tolerance)
+
+test_model_feature_matrix, test_output = get_numpy_data(test_data, input_features, output_feature)
+model_predictions_test = predict_outcome(test_model_feature_matrix, model_weights)
+# print model_prediction_test[0], test_output[0]
+
+
+# data: feature_matrix
+def get_residual_sum_of_squares(data, outcome, weights):
+    predictions = predict_outcome(data, weights)
+    residuals = outcome - predictions
+    rss = (residuals*residuals).sum()
+    return rss
+
+model_rss = get_residual_sum_of_squares(test_model_feature_matrix, test_output, model_weights)
+print model_rss
